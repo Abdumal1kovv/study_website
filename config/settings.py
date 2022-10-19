6b75d7d96dde2ceb957a71104186b33865e9e8f5
Abdumalikov Abbosxo'ja <100085152+Abdumal1kovv@users.noreply.github.com>
2022-10-19 22:15:14 +0500

    Some changes

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', 'study-website.herokuapp.com']
 
 # Application definition
 
